1a81ab2c07ae2bef458618f8500b3b066b20a635
beomshin.kim <chzh859@naver.com>
2024-05-27 20:37:02 +0900

    # 기본 스프링 구조 생성

---
+++ src/main/java/com/kr/piece/test/HealthController.java
@@ -0,0 +1,18 @@
+package com.kr.piece.test;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "success";
+    }
+
+}
